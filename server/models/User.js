019674b48f61b1759b12f9c50b054aabd4215f03
jeffch19 <17jcummings@gmail.com>
2024-03-05 20:43:59 -0500

    fixed comments

--- server/models/User.js
+++ server/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
+    // set to be an array of data that adheres to the bookSchema
     journel: [journelSchema],
   },
   // set this to use virtual below
