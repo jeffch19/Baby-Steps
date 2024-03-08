3950f9a134786d788304397e9f4a3c5b18b339ec
jeffch19 <17jcummings@gmail.com>
2024-03-07 20:15:16 -0500

    added calendar

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css'; // Import the CSS for the calendar
+
+const CalendarComponent = () => {
+ return (
+    <div>
+      <Calendar />
+    </div>
+ );
+};
+
+export default CalendarComponent;
