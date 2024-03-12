b0c4f971f7fa695f089ebf166c4029dd8c22d610
Helen Colon <helenvcolon@gmail.com>
2024-03-12 15:38:51 -0400

    Added images, created folder css, footer css and home css, edited app.jsx home.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import bouquetLeft from '../assets/bouquetLeft.png';
+import bouquetRight from '../assets/bouquetRight.png';
+import blueFetus from '../assets/blueFetus.png';
+import '../css/footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <img src={bouquetLeft} alt="bouquet left" className="footer-icon" />
+        <p className="footer-text">Created with</p>
+        <img src={blueFetus} alt="blue heart and fetus" className="footer-image" />
+        <p className="footer-text">by JAHZ</p>
+        <img src={bouquetRight} alt="bouquet right" className="footer-icon" />
+      </div>
+    </footer>
+  );
+};
+
+
+export default Footer;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import '../css/home.css';
 export default function Home() {
     return (
         <motion.div
@@ -8,7 +9,7 @@ export default function Home() {
             transition={{ duration: 0.5, ease: 'easeOut' }}>
             <div className='mainpage'>
                 <h1 className='text-black text-center pt-8'>My Pregnancy Journal</h1>
-                <p className='text-black text-center'>Where your dreams come true</p>
+                <p className='text-black text-center'>Every moment, a precious keepsake.</p>
             </div>
         </motion.div>
     )
