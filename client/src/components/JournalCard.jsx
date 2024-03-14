42f82e2887c74d75dcae38e159701190a6d6d328
Zion Griffin <defendingmymaininbox@gmail.com>
2024-03-13 21:05:13 -0400

    Journal query is almost done? Not tested yet

--- client/src/components/JournalCard.jsx
+++ client/src/components/JournalCard.jsx
@@ -4,22 +4,42 @@ import { useMutation } from '@apollo/client';
 
 export default function JournalCard() {
     const [formState, setFormState] = useState({
-        title: '', date: '', weight: '', months: ''
+        title: '', date: '', weight: '', months: '', body: ''
     });
     const [submission, { error, data }] = useMutation(ADD_Journel);
 
     const handleFormSubmit = async(e) => {
         e.preventDefault();
+        console.log(formState);
+        try {
+            const { data } = await submission({
+                variables: { ...formState },
+            });
+            console.log(data);
+        }
+        catch (e) {
+            console.error(e);
+        }
+        setFormState({
+            title: '', date: '', weight: '', months: '', body: ''
+        })
     }
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
     return (
         <div className='mx-auto w-6/12 text-black my-3 h-[75vh]'>
             <form onSubmit={handleFormSubmit}>
                 <div className='flex flex-col'>
-                    <input className="my-2 bg-white" name='title' placeholder='Title'></input>
-                    <input className="my-2 bg-white" name='date' placeholder='Date'></input>
-                    <input className="my-2 bg-white" name='weight' placeholder='Weight'></input>
+                    <input className="my-2 bg-white" name='title' placeholder='Title' onChange={handleChange}></input>
+                    <input className="my-2 bg-white" name='date' placeholder='Date' onChange={handleChange}></input>
+                    <input className="my-2 bg-white" name='weight' placeholder='Weight' onChange={handleChange}></input>
                     {/* <input list='months'></input> */}
-                    <select name='months' className='bg-white my-2 text-black'>
+                    <select name='months' className='bg-white my-2 text-black' onChange={handleChange}>
                         <option value='' disabled selected>Select a month</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -32,7 +52,7 @@ export default function JournalCard() {
                         <option value="9">9</option>
                     </select>
                 </div>
-                <textArea className='w-full bg-white' name='body' placeholder='Type thoughts/events here...' rows='8' />
+                <textArea className='w-full bg-white' name='body' placeholder='Type thoughts/events here...' rows='8' onChange={handleChange}/>
                 <button type='submit'>Submit</button>
             </form>
         </div>
