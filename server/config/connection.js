a72d2675186043f10eb423a037dd1b03aa1f9015
jeffch19 <17jcummings@gmail.com>
2024-03-14 20:48:59 -0400

    testing something

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 //need to configure this with mongoose before we deploy
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://project3:babysteps@cluster0.ockyvl0.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/babysteps');
 
 module.exports = mongoose.connection;
